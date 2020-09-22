01810a27f9fb763e3fdc13f9da1117f5026ca567
levantuanpxu <70925631+levantuanpxu@users.noreply.github.com>
2020-09-22 10:48:23 +0700

    Static_Method
    Static_Method

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace Static_Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student.Change();//calling change method
+                             //creating objects
+            Student s1 = new Student(00, "ok");
+            Student s2 = new Student(99, "ola");
+            Student s3 = new Student(88, "ola");
+            //calling display method
+            s1.Display();
+            s2.Display();
+            s3.Display();
+        }
+    }
+}
